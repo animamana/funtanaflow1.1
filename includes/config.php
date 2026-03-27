40065e8f0d3f8b191d35739bcfc8fa7dd125feef
animamana <animapaolo@gmail.com>
2026-03-27 17:01:02 +0100

    Creating configuration file for language and site settings

---
+++ includes/config.php
@@ -0,0 +1,22 @@
+<?php
+
+// Language settings
+$lang_definitions = [
+    'en' => [
+        'welcome' => 'Welcome',
+        'site_name' => 'My Site',
+    ],
+    'es' => [
+        'welcome' => 'Bienvenido',
+        'site_name' => 'Mi Sitio',
+    ],
+];
+
+// Site settings
+$site_settings = [
+    'default_language' => 'en',
+    'timezone' => 'UTC',
+    'site_description' => 'A brief description of the site',
+];
+
+?>
