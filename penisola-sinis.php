c93654e917afee727c2f09f3ef958e3f5eb294fd
animamana <animapaolo@gmail.com>
2026-03-27 17:15:31 +0100

    Create penisola-sinis.php with multilingual content about exploring the Sinis Peninsula

---
+++ penisola-sinis.php
@@ -0,0 +1,24 @@
+<?php
+
+$multilingual_content = [
+    'it' => [
+        'title' => 'Esplorando la Penisola del Sinis',
+        'tharros' => 'Tharros è un antico sito fenicio che offre splendide rovine e sculture.',
+        'monteprama' => 'Monteprama è famoso per le sue statue in pietra risalenti al IX secolo a.C.',
+        'san_salvatore' => 'San Salvatore è un luogo di culto e offre una vista panoramica sulla costa.',
+    ],
+    'en' => [
+        'title' => 'Exploring the Sinis Peninsula',
+        'tharros' => 'Tharros is an ancient Phoenician site featuring beautiful ruins and sculptures.',
+        'monteprama' => 'Monteprama is famous for its stone statues dating back to the 9th century BC.',
+        'san_salvatore' => 'San Salvatore is a place of worship that offers panoramic views of the coast.',
+    ],
+    'de' => [
+        'title' => 'Erkundung der Sinis-Halbinsel',
+        'tharros' => 'Tharros ist eine alte phönizische Stätte mit schönen Ruinen und Skulpturen.',
+        'monteprama' => 'Monteprama ist berühmt für seine Steinstatuen aus dem 9. Jahrhundert v. Chr.',
+        'san_salvatore' => 'San Salvatore ist ein Ort der Verehrung und bietet einen Panoramablick auf die Küste.',
+    ],
+];
+
+?>
