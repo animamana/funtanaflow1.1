5a1134724d43391cf3a22018843c77bc38e784d8
animamana <animapaolo@gmail.com>
2026-03-27 17:03:25 +0100

    Add footer HTML structure with contact form and social media links

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <div class="contact">
+        <h2>Contact Us</h2>
+        <form>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+            <button type="submit">Send</button>
+        </form>
+    </div>
+    <div class="social-media">
+        <h2>Follow Us</h2>
+        <a href="https://facebook.com" target="_blank">Facebook</a>
+        <a href="https://twitter.com" target="_blank">Twitter</a>
+        <a href="https://instagram.com" target="_blank">Instagram</a>
+    </div>
+</footer>
